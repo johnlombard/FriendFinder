8810d69818148822e3003957c9b5f39432e4ef8b
John Lombard <John@Johns-MBP.fios-router.home>
2018-10-31 16:07:16 -0400

    Routes working

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 // Dependencies
 var express = require("express");
 var path = require("path");
+
+
+
+// Dependencies
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
